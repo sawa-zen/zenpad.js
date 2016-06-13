56b44ecb3289903cb77fe3bca6ccf9860b352c72
sawada <sawadasuiren@gmail.com>
2016-06-13 23:42:38 +0900

    リサイズ処理追加

--- src/Zenpad.ts
+++ src/Zenpad.ts
@@ -12,6 +12,11 @@ class Zenpad {
   /** stage */
   private _stage:createjs.Stage;
 
+  /** 左側グループ */
+  private _leftButtons:createjs.Container;
+  /** 右側グループ */
+  private _rightButtons:createjs.Container;
+
   /**
    * コンストラクター
    * @constructor
@@ -22,28 +27,53 @@ class Zenpad {
     // ステージを作成
     this._canvasId = canvasId;
     this._canvas = document.getElementById(this._canvasId);
-    this._canvas.setAttribute('width', String(this._canvas.clientWidth));
-    this._canvas.setAttribute('height', String(this._canvas.clientHeight));
 
+    // ステージ
     this._stage = new createjs.Stage(this._canvasId);
-    // this._stage.canvas.width = this._canvas.clientWidth;
-    // this._stage.canvas.height = this._canvas.clientHeight;
     createjs.Touch.enable(this._stage);
+
+    // 左側グループ
+    this._leftButtons = new createjs.Container();
+    this._stage.addChild(this._leftButtons);
+
+    // 右側
+    this._rightButtons = new createjs.Container();
+    this._rightButtons.regX = 200;
+    this._stage.addChild(this._rightButtons);
 
     // ボタン
     let button = new Button();
-    this._stage.addChild(button);
+    button.x = 120;
+    button.y = 30;
+    this._rightButtons.addChild(button);
 
     // アニメーション
     createjs.Ticker.addEventListener("tick", () => this._tick());
+
+    // リサイズ
+    window.addEventListener("resize", () => this._resize());
+
+    // 初回リサイズ処理
+    this._resize();
   }
 
   /**
    * 毎フレーム毎のアニメーション
    */
   private _tick() {
-    console.info("aaa");
     this._stage.update();
+  }
+
+  /**
+   * リサイズ
+   */
+  private _resize() {
+    // canvasサイズを合わせる
+    this._canvas.setAttribute('width', String(this._canvas.clientWidth));
+    this._canvas.setAttribute('height', String(this._canvas.clientHeight));
+
+    // 右側グループを右隅に
+    this._rightButtons.x = this._canvas.clientWidth;
   }
 }
 
