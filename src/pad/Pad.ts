2c52f9ac16993b9d740124c309af705a46c7ac82
sawada <sawadasuiren@gmail.com>
2016-06-15 22:14:18 +0900

    padが枠外にはみ出ないように制御

--- src/pad/Pad.ts
+++ src/pad/Pad.ts
@@ -3,6 +3,11 @@
  */
 export default class Pad extends createjs.Container {
 
+  /** 半径 */
+  private _radius:number = 60;
+  /** スティクの半径 */
+  private _stickRadius:number = 30;
+
   /** スティック */
   private _stick:createjs.Shape;
   /** 操作中 */
@@ -18,14 +23,14 @@ export default class Pad extends createjs.Container {
     // 背景
     let bg = new createjs.Shape();
     bg.graphics.beginFill('#abafb8');
-    bg.graphics.drawCircle(0, 0, 60);
+    bg.graphics.drawCircle(0, 0, this._radius);
     bg.graphics.endFill();
     this.addChild(bg);
 
     // スティック
     this._stick = new createjs.Shape();
     this._stick.graphics.beginFill('#333333');
-    this._stick.graphics.drawCircle(0, 0, 30);
+    this._stick.graphics.drawCircle(0, 0, this._stickRadius);
     this._stick.graphics.endFill();
     this.addChild(this._stick);
 
@@ -38,9 +43,17 @@ export default class Pad extends createjs.Container {
    * タッチムーブ時のハンドラーです。
    */
   private _onTouchMove(event:createjs.MouseEvent) {
-    console.info(event.localX, event.localY);
-    this._stick.x = event.localX;
-    this._stick.y = event.localY;
+    let x = event.localX,
+        y = event.localY;
+    let vec = new Victor(x, y);
+    if(vec.length() > this._radius - this._stickRadius) {
+      let v = vec.normalize().multiplyScalar(this._radius - this._stickRadius);
+      this._stick.x = v.x;
+      this._stick.y = v.y;
+    } else {
+      this._stick.x = event.localX;
+      this._stick.y = event.localY;
+    }
   }
 
   /**
