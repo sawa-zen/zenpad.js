56a4ee4cb514838b5410313b59b7032a807d4cd5
sawada <sawadasuiren@gmail.com>
2016-12-27 18:59:18 +0900

    PixiJSが動く状態に

--- src/Zenpad.js
+++ src/Zenpad.js
@@ -1,85 +1,84 @@
-import Button from './component/Button';
-import Pad from './component/Pad';
-import EventName from './event/EventName';
-import PublicEventName from './event/PublicEventName';
+import * as PIXI from 'pixi.js';
+// import Button from './component/Button';
+// import Pad from './component/Pad';
+// import EventName from './event/EventName';
+//import PublicEventName from './event/PublicEventName';
 
 /**
  * Zenpadのメインクラスです。
  */
-class Zenpad extends createjs.EventDispatcher {
+class Zenpad extends PIXI.utils.EventEmitter {
 
   /**
    * コンストラクター
    * @constructor
-   * @param {string} dpmId
    */
-  constructor(canvasId) {
+  constructor(wrapperId, width, height) {
     super();
 
-    // ステージを作成
-    this._canvasId = canvasId;
-    this._canvas = document.getElementById(this._canvasId);
+    // ラッパーを取得
+    this._wrapper = document.getElementById(wrapperId);
 
-    // ステージ
-    this._stage = new createjs.Stage(this._canvasId);
-    createjs.Touch.enable(this._stage);
+    // レンダラー
+    this._renderer = PIXI.autoDetectRenderer(this._wrapper.offsetWidth, this._wrapper.offsetHeight, { transparent: true });
+    this._wrapper.append(this._renderer.view);
 
-    // 左側グループ
-    this._leftButtons = new createjs.Container();
-    this._stage.addChild(this._leftButtons);
-
-    // 右側グループ
-    this._rightButtons = new createjs.Container();
-    this._rightButtons.regX = 190;
-    this._stage.addChild(this._rightButtons);
-
-    // アナログパッド
-    let pad = new Pad();
-    pad.x = 80;
-    pad.y = 90;
-    this._leftButtons.addChild(pad);
-
-    // Aボタン
-    let aButton = new Button(PublicEventName.CLICK_A);
-    aButton.x = 150;
-    aButton.y = 80;
-    this._rightButtons.addChild(aButton);
-
-    // Bボタン
-    let bButton = new Button(PublicEventName.CLICK_B);
-    bButton.x = 80;
-    bButton.y = 110;
-    this._rightButtons.addChild(bButton);
-
-    // フレーム毎の更新
-    this._tick = this._tick.bind(this);
-    createjs.Ticker.addEventListener("tick", this._tick);
-
-    // リサイズ
-    this._resize = this._resize.bind(this);
-    window.addEventListener("resize", this._resize);
-
-    // 初回リサイズ処理
-    this._resize();
+    // // 左側グループ
+    // this._leftButtons = new createjs.Container();
+    // this._stage.addChild(this._leftButtons);
+    //
+    // // 右側グループ
+    // this._rightButtons = new createjs.Container();
+    // this._rightButtons.regX = 190;
+    // this._stage.addChild(this._rightButtons);
+    //
+    // // アナログパッド
+    // let pad = new Pad();
+    // pad.x = 80;
+    // pad.y = 90;
+    // this._leftButtons.addChild(pad);
+    //
+    // // Aボタン
+    // let aButton = new Button(PublicEventName.CLICK_A);
+    // aButton.x = 150;
+    // aButton.y = 80;
+    // this._rightButtons.addChild(aButton);
+    //
+    // // Bボタン
+    // let bButton = new Button(PublicEventName.CLICK_B);
+    // bButton.x = 80;
+    // bButton.y = 110;
+    // this._rightButtons.addChild(bButton);
+    //
+    // // フレーム毎の更新
+    // this._tick = this._tick.bind(this);
+    // createjs.Ticker.addEventListener("tick", this._tick);
+    //
+    // // リサイズ
+    // this._resize = this._resize.bind(this);
+    // window.addEventListener("resize", this._resize);
+    //
+    // // 初回リサイズ処理
+    // this._resize();
   }
 
   /**
    * 毎フレーム毎のアニメーション
    */
   _tick() {
-    this._stage.update();
+    // this._stage.update();
   }
 
   /**
    * リサイズ
    */
   _resize() {
-    // canvasサイズを合わせる
-    this._canvas.setAttribute('width', String(this._canvas.clientWidth));
-    this._canvas.setAttribute('height', String(this._canvas.clientHeight));
-
-    // 右側グループを右隅に
-    this._rightButtons.x = this._canvas.clientWidth;
+    // // canvasサイズを合わせる
+    // this._canvas.setAttribute('width', String(this._canvas.clientWidth));
+    // this._canvas.setAttribute('height', String(this._canvas.clientHeight));
+    //
+    // // 右側グループを右隅に
+    // this._rightButtons.x = this._canvas.clientWidth;
   }
 }
 
