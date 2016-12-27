905f966ae591b8c345c9aeccb84cacf5a42ddb2a
sawada <sawadasuiren@gmail.com>
2016-12-27 16:27:22 +0900

    Button.jsを修正

--- src/component/Button.js
+++ src/component/Button.js
@@ -6,28 +6,22 @@ import EventManager from '../event/EventManager';
  */
 export default class Button extends createjs.Container {
 
-  /** クリック時のイベント名 */
-  private _eventName:string;
-
-  /** 幅 */
-  private _width:number = 55;
-  /** シェイプ */
-  private _shape:createjs.Shape;
-
-  /** イベントマネージャー */
-  private _eventManager:EventManager;
-
   /**
    * コンストラクター
    * @constructor
    */
-  constructor(eventName:string) {
+  constructor(eventName) {
     super();
+
+    // 幅
+    this._width = 55;
 
     // イベント名
     this._eventName = eventName;
     // イベントマネージャー
     this._eventManager = EventManager.getInstance();
+
+    this._onClick = this._onClick.bind(this);
 
     // シェイプ
     this._shape = new createjs.Shape();
@@ -35,15 +29,13 @@ export default class Button extends createjs.Container {
     this._shape.graphics.drawCircle(0, 0, this._width / 2);
     this._shape.graphics.endFill();
     this.addChild(this._shape);
-    this._onClick = this._onClick.bind(this);
     this._shape.addEventListener(EventName.CLICK, this._onClick);
   }
 
   /**
    * ボタン押下時のハンドラーです。
    */
-  private _onClick(event:createjs.MouseEvent):void {
+  _onClick(event) {
     this._eventManager.dispatch(this._eventName);
   }
-
 }
