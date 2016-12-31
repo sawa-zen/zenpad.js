753dcc4717c3fda0a03f0bc5b63555b905c5f30e
sawada <sawadasuiren@gmail.com>
2016-12-31 21:47:50 +0900

    スティックの動作を修正

--- src/component/Pad.js
+++ src/component/Pad.js
@@ -16,16 +16,31 @@ export default class Pad extends PIXI.Container {
     this._radius = 60;
     // スティクの半径
     this._stickRadius = 30;
+    // ドラッグ中かどうか
+    this._isDragging = false;
 
+    this.width = 300;
+    this.height = 300;
+    this.interactive = true;
+
+    this._onClick = this._onClick.bind(this);
     this._onTouchMove = this._onTouchMove.bind(this);
     this._onMouseUp = this._onMouseUp.bind(this);
+
+    // タップエリア
+    this._tapArea = new PIXI.Graphics();
+    this._tapArea.beginFill(0xff0000);
+    this._tapArea.drawRect(-250, -250, 500, 500);
+    this._tapArea.endFill();
+    this._tapArea.alpha = 0;
+    this._tapArea.interactive = true;
+    this.addChild(this._tapArea);
 
     // 背景
     this._bg = new PIXI.Graphics();
     this._bg.beginFill(0xabafb8);
     this._bg.drawCircle(0, 0, this._radius);
     this._bg.endFill();
-    this._bg.interactive = true;
     this.addChild(this._bg);
 
     // スティック
@@ -33,22 +48,36 @@ export default class Pad extends PIXI.Container {
     this._stick.beginFill(0x333333);
     this._stick.drawCircle(0, 0, this._stickRadius);
     this._stick.endFill();
+    this._stick.interactive = true;
     this._stick.buttonMode = true;
     this.addChild(this._stick);
 
-    // タッチイベント
-    this._bg.on('mousemove', this._onTouchMove);
-    this._bg.on('mouseup', this._onMouseUp);
+    this._stick.on('mousedown', this._onClick);
+    this._stick.on('mousemove', this._onMouseMoveStick);
+    this.on('mouseup', this._onMouseUp);
+    this.on('mousemove', this._onTouchMove);
+  }
+
+  /**
+   * クリック時のハンドラーです。
+   */
+  _onClick() {
+    this._isDragging = true;
   }
 
   /**
    * タッチムーブ時のハンドラーです。
    */
   _onTouchMove(event) {
+
+    // ドラッグ中でなければ処理しない
+    if(!this._isDragging) {
+      return;
+    }
+
     let posision = event.data.getLocalPosition(this);
     let x = posision.x,
         y = posision.y;
-    console.info(x, y);
     let vec = new Victor(x, y);
 
     // 枠外に出ていれば枠内に収める
@@ -88,8 +117,13 @@ export default class Pad extends PIXI.Container {
    * マウスアップ時のハンドラーです。
    */
   _onMouseUp() {
+    // ドラッグフラグを折る
+    this._isDragging = false;
+
+    // スティックをもとの位置に戻す
     this._stick.x = 0;
     this._stick.y = 0;
-    this._currentDirection = null;
+
+    //this._currentDirection = null;
   }
 }
