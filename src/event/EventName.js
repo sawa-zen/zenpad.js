65e71d8641d1eb8d3a4aed64db68b4cc9f3dcf38
sawada <sawadasuiren@gmail.com>
2017-01-03 17:38:32 +0900

    EventNameに追加

--- src/event/EventName.js
+++ src/event/EventName.js
@@ -2,7 +2,11 @@
  * イベント名一覧
  */
 export default class EventName {
-  static get CLICK()      { return 'click'; };
-  static get STICK_MOVE() { return 'stick_move'; };
-  static get TOUCH_END()  { return 'touchend'; };
+  static get CLICK()       { return 'click'; };
+  static get STICK_MOVE()  { return 'stick_move'; };
+  static get MOUSE_UP()    { return 'mouseup'; };
+  static get MOUSE_MOVE()  { return 'mousemove'; };
+  static get TOUCH_START() { return 'touchstart'; };
+  static get TOUCH_END()   { return 'touchend'; };
+  static get TOUCH_MOVE()  { return 'touchmove'; };
 }
