fed13950f0dd63d4bd94c442de5f35c49de48952
sawada <sawadasuiren@gmail.com>
2017-01-03 22:23:33 +0900

    ボタンに文字が乗るように実装

--- src/component/Button.js
+++ src/component/Button.js
@@ -16,6 +16,7 @@ export default class Button extends PIXI.Container {
     this._width = 55;
 
     this._onClick = this._onClick.bind(this);
+    this._onTouchStart = this._onTouchStart.bind(this);
 
     // シェイプ
     this._shape = new PIXI.Graphics();
@@ -25,7 +26,20 @@ export default class Button extends PIXI.Container {
     this._shape.interactive = true;
     this._shape.buttonMode = true;
     this.addChild(this._shape);
+
+    // テキスト
+    let text = new PIXI.Text('B', {
+      font: '30px Arial',
+      fill: 0xffffff
+    });
+    text.x = -10;
+    text.y = -16;
+    text.alpha = 0.2;
+    this._shape.addChild(text);
+
+    // イベント
     this._shape.on(EventName.CLICK, this._onClick);
+    this._shape.on(EventName.TOUCH_START, this._onTouchStart);
     this._shape.on(EventName.TOUCH_END, this._onClick);
   }
 
@@ -35,5 +49,13 @@ export default class Button extends PIXI.Container {
   _onClick(event) {
     // クリックイベントを発火
     this.emit(EventName.CLICK);
+  }
+
+  /**
+   * タッチスタート
+   */
+  _onTouchStart() {
+    // ボタンを凹ます
+
   }
 }
