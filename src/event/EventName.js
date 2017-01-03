cac051d1e822f83a6527e350dfc8637103f050ae
sawada <sawadasuiren@gmail.com>
2017-01-04 02:24:17 +0900

    MouseUpOutsideイベントを追加

--- src/event/EventName.js
+++ src/event/EventName.js
@@ -6,6 +6,7 @@ export default class EventName {
   static get STICK_MOVE()  { return 'stick_move'; };
   static get MOUSE_UP()    { return 'mouseup'; };
   static get MOUSE_MOVE()  { return 'mousemove'; };
+  static get MOUSE_UP_OUTSIDE() { return 'mouseupoutside'; };
   static get TOUCH_START() { return 'touchstart'; };
   static get TOUCH_END()   { return 'touchend'; };
   static get TOUCH_MOVE()  { return 'touchmove'; };
