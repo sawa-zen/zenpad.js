4149e9760e096168eb16a418b0e590ae031f3b42
sawada <sawadasuiren@gmail.com>
2017-01-04 02:45:16 +0900

    MouseDownイベントを追加

--- src/event/EventName.js
+++ src/event/EventName.js
@@ -4,6 +4,7 @@
 export default class EventName {
   static get CLICK()       { return 'click'; };
   static get STICK_MOVE()  { return 'stick_move'; };
+  static get MOUSE_DOWN()  { return 'mousedown'; };
   static get MOUSE_UP()    { return 'mouseup'; };
   static get MOUSE_MOVE()  { return 'mousemove'; };
   static get MOUSE_UP_OUTSIDE() { return 'mouseupoutside'; };
